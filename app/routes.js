6674fa288407bc49f62c451146eef3f482c7b21e
Neil <neil.holroyd@engineering.digital.dwp.gov.uk>
2023-11-23 15:35:14 +0000

    Sprint 7 changes to prototype

--- app/routes.js
+++ app/routes.js
@@ -48,11 +48,12 @@ router.post('/choose-programme_2b', function (req, res) {
   } else if (answer === 'whp-m') {
     // Send user to whp m page
     res.redirect('identify-match-refer/V1_2-1/whp-core/whp-m')
-  } else if (answer === 'whp-mee') {
+  } else {
     // Send user to whp mee page
     res.redirect('identify-match-refer/V1_2-1/whp-core/whp-mee')
   }
 });
+
 
 /////////////////  RESTART ///////////////
 // run this code when a form is submitted to 'address-a' page
@@ -80,7 +81,7 @@ router.post('/address_2-1-whp', function (req, res) {
   if (answer === 'correct') {
     // Send user to Are contact details correct?
     res.redirect('identify-match-refer/V1_2-1/whp-core/phone-a')
-  } else if (answer === 'incorrect') {
+  } else {
     // Send user to You must use LMS to make a referral
     res.redirect('identify-match-refer/V1_2/not-suitable-address')
   }
@@ -94,15 +95,71 @@ router.post('/address_2b-pioneer', function (req, res) {
   console.log(answer, 'submitted-answer')
 
   if (answer === 'correct') {
-    // Send user to Are contact details correct?
     res.redirect('identify-match-refer/V1_2-1/whp-pioneer/contact-details-pioneer')
+  } else if (answer === 'incorrect') {
+    res.redirect('identify-match-refer/V1_2/not-suitable-address')
+  } else {
+    res.redirect('/identify-match-refer/V1_2-1/other-files/address-pioneer-error')
+  }
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////// DUPLICATE REFERRALS – AVAILABLE OPPORTUNITIES ////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+// run this code when a form is submitted to 'available-opportunities' page on the duplicate referral journey
+
+router.post('/choose-programme_duplicate_journey', function (req, res) {
+  var answer = req.session.data['select-programme']
+  console.log(answer, 'submitted-answer')
+
+  if (answer === 'restart') {
+    // Send user to restart-suitability page
+    res.redirect('identify-match-refer/V1_2-1/restart/restart')
+  } else if (answer === 'pioneer-dc') {
+    // Send user to pioneer dc page
+    res.redirect('identify-match-refer/V1_2-1/duplicate-referral/whp-pioneer/pioneer-dc')
+  } else if (answer === 'pioneer-ee') {
+    // Send user to pioneer ee page
+    res.redirect('identify-match-refer/V1_2-1/whp-pioneer/pioneer-ee')
+  } else if (answer === 'pioneer-m') {
+    // Send user to pioneer m page
+    res.redirect('identify-match-refer/V1_2-1/whp-pioneer/pioneer-m')
+  } else if (answer === 'pioneer-mee') {
+    // Send user to pioneer mee page
+    res.redirect('identify-match-refer/V1_2-1/whp-pioneer/pioneer-mee')
+  } else if (answer === 'whp-dc') {
+    // Send user to whp dc page
+    res.redirect('identify-match-refer/V1_2-1/whp-core/whp-dc')
+  } else if (answer === 'whp-ee') {
+    // Send user to whp ee page
+    res.redirect('identify-match-refer/V1_2-1/whp-core/whp-ee')
+  } else if (answer === 'whp-m') {
+    // Send user to whp m page
+    res.redirect('identify-match-refer/V1_2-1/whp-core/whp-m')
+  } else if (answer === 'whp-mee') {
+    // Send user to whp mee page
+    res.redirect('identify-match-refer/V1_2-1/whp-core/whp-mee')
+  }
+});
+
+/////////////////  WHP-PIONEER – DUPICATE REFERRALS ///////////////
+// run this code when a form is submitted to 'address-pioneer' page
+
+router.post('/address_pioneer_duplicate_referral', function (req, res) {
+  var answer = req.session.data['correct-address']
+  console.log(answer, 'submitted-answer')
+
+  if (answer === 'correct') {
+    // Send user to Are contact details correct?
+    res.redirect('identify-match-refer/V1_2-1/duplicate-referral/whp-pioneer/contact-details-pioneer')
   } else if (answer === 'incorrect') {
     // Send user to You must use LMS to make a referral
     res.redirect('identify-match-refer/V1_2/not-suitable-address')
   }
 });
-
-
 
 ///////////////////////////////////////////////  OLDER VERSIONS /////////////////////////////////////////////
 
